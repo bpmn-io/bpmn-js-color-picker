43726b0c0e93e67f51ec4c846c121bc998eafb4b
Nico Rehwaldt <git_nikku@nixis.de>
2022-12-06 22:05:49 +0100

    chore: extract default colors into constant

--- colors/ColorPopupProvider.js
+++ colors/ColorPopupProvider.js
@@ -1,3 +1,30 @@
+const COLORS = [ {
+  label: 'Default',
+  fill: undefined,
+  stroke: undefined
+}, {
+  label: 'Blue',
+  fill: '#BBDEFB',
+  stroke: '#0D4372'
+}, {
+  label: 'Orange',
+  fill: '#FFE0B2',
+  stroke: '#6B3C00'
+}, {
+  label: 'Green',
+  fill: '#C8E6C9',
+  stroke: '#205022'
+}, {
+  label: 'Red',
+  fill: '#FFCDD2',
+  stroke: '#831311'
+}, {
+  label: 'Purple',
+  fill: '#E1BEE7',
+  stroke: '#5B176D'
+} ];
+
+
 export default function ColorPopupProvider(popupMenu, modeling, translate) {
   this._popupMenu = popupMenu;
   this._modeling = modeling;
@@ -17,33 +44,8 @@ ColorPopupProvider.$inject = [
 ColorPopupProvider.prototype.getEntries = function(element) {
   var self = this;
 
-  var colors = [ {
-    label: 'Default',
-    fill: undefined,
-    stroke: undefined
-  }, {
-    label: 'Blue',
-    fill: '#BBDEFB',
-    stroke: '#0D4372'
-  }, {
-    label: 'Orange',
-    fill: '#FFE0B2',
-    stroke: '#6B3C00'
-  }, {
-    label: 'Green',
-    fill: '#C8E6C9',
-    stroke: '#205022'
-  }, {
-    label: 'Red',
-    fill: '#FFCDD2',
-    stroke: '#831311'
-  }, {
-    label: 'Purple',
-    fill: '#E1BEE7',
-    stroke: '#5B176D'
-  } ];
+  var entries = COLORS.map(function(color) {
 
-  var entries = colors.map(function(color) {
     return {
       title: self._translate(color.label),
       id: color.label.toLowerCase() + '-color',
