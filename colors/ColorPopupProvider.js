5b302749be8fdde9470279256cfbb2080235214f
zxiaozhou <z7630853@163.com>
2023-04-29 18:20:43 +0800

    feat: Change how icons are loaded and fit in dark mode
    1. Change the icon loading mode to html so that you can change the color
    using the css
    2. Change the color's default fill color and stroke color
    by adding a theme configuration to fit the dark mode

--- colors/ColorPopupProvider.js
+++ colors/ColorPopupProvider.js
@@ -1,52 +1,59 @@
-import {
-  domify
-} from 'min-dom';
+import { domify } from 'min-dom';
 
-const COLORS = [ {
-  label: 'Default',
-  fill: undefined,
-  stroke: undefined
-}, {
-  label: 'Blue',
-  fill: '#BBDEFB',
-  stroke: '#0D4372'
-}, {
-  label: 'Orange',
-  fill: '#FFE0B2',
-  stroke: '#6B3C00'
-}, {
-  label: 'Green',
-  fill: '#C8E6C9',
-  stroke: '#205022'
-}, {
-  label: 'Red',
-  fill: '#FFCDD2',
-  stroke: '#831311'
-}, {
-  label: 'Purple',
-  fill: '#E1BEE7',
-  stroke: '#5B176D'
-} ];
+const COLORS = [
+  {
+    label: 'Default',
+    fill: undefined,
+    stroke: undefined,
+  },
+  {
+    label: 'Blue',
+    fill: '#BBDEFB',
+    stroke: '#0D4372',
+  },
+  {
+    label: 'Orange',
+    fill: '#FFE0B2',
+    stroke: '#6B3C00',
+  },
+  {
+    label: 'Green',
+    fill: '#C8E6C9',
+    stroke: '#205022',
+  },
+  {
+    label: 'Red',
+    fill: '#FFCDD2',
+    stroke: '#831311',
+  },
+  {
+    label: 'Purple',
+    fill: '#E1BEE7',
+    stroke: '#5B176D',
+  },
+];
 
-
-export default function ColorPopupProvider(config, popupMenu, modeling, translate) {
+export default function ColorPopupProvider(
+    config,
+    popupMenu,
+    modeling,
+    translate
+) {
   this._popupMenu = popupMenu;
   this._modeling = modeling;
   this._translate = translate;
 
-  this._colors = config && config.colors || COLORS;
-
+  this._colors = (config && config.colors) || COLORS;
+  this._theme = (config && config.theme) || 'light';
   this._popupMenu.registerProvider('color-picker', this);
 }
-
 
 ColorPopupProvider.$inject = [
   'config.colorPicker',
   'popupMenu',
   'modeling',
-  'translate'
+  'translate',
 ];
-
 
 ColorPopupProvider.prototype.getEntries = function(elements) {
   var self = this;
@@ -56,23 +63,29 @@ ColorPopupProvider.prototype.getEntries = function(elements) {
       <rect rx="2" x="1" y="1" width="22" height="22" fill="var(--fill-color)" stroke="var(--stroke-color)"></rect>
     </svg>
   `);
-
+  const theme = (this._theme && this._theme) || 'light';
+  const defaultFill = theme == 'light' ? 'white' : 'black';
+  const defaultStroke =
+    theme == 'light' ? 'rgb(34, 36, 42)' : 'rgb(221, 219, 213)';
   var entries = this._colors.map(function(color) {
-
-    colorIcon.style.setProperty('--fill-color', color.fill || 'white');
-    colorIcon.style.setProperty('--stroke-color', color.stroke || 'rgb(34, 36, 42)');
+    colorIcon.style.setProperty('--fill-color', color.fill || defaultFill);
+    colorIcon.style.setProperty(
+      '--stroke-color',
+      color.stroke || defaultStroke
+    );
 
     return {
       title: self._translate(color.label),
       id: color.label.toLowerCase() + '-color',
-      imageUrl: `data:image/svg+xml;utf8,${ encodeURIComponent(colorIcon.outerHTML) }`,
-      action: createAction(self._modeling, elements, color)
+      imageUrl: `data:image/svg+xml;utf8,${encodeURIComponent(
+        colorIcon.outerHTML
+      )}`,
+      action: createAction(self._modeling, elements, color),
     };
   });
 
   return entries;
 };
-
 
 function createAction(modeling, element, color) {
   return function() {
